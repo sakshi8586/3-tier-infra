9b142cef22b60aa286587d17d57565cd76e4e9ac
Sakshi <156531810+sakshi8586@users.noreply.github.com>
2025-08-11 20:46:38 +0530

    add infra.tf

---
+++ infra.tf
@@ -0,0 +1,12 @@
+variable "rg_name" {
+  default = "three-tier-rg"
+}
+variable "location" {
+  default = "eastus"
+}
+variable "acr_name" {
+  default = "threetieracr"
+}
+variable "aks_name" {
+  default = "three-tier-aks"
+}
